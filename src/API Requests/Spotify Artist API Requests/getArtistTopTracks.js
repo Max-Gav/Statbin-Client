48de1a6925c06c3137f29b138deafc363f585e5d
AFROJOKER <maximgav123@gmail.com>
2023-10-28 22:22:23 +0300

    Created a method that fetches the user top tracks

---
+++ src/API Requests/Spotify Artist API Requests/getArtistTopTracks.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const getArtistTopTracks = async (accessToken, setData, url) => {
+
+    if(accessToken){
+        const response = await axios({
+            method: "get",
+            url: url + "/top-tracks?market=US",
+            headers: {
+              Authorization: "Bearer " + accessToken,
+            },
+          })
+          setData(response.data.tracks)
+        
+    }
+    else{
+        console.log("No Access Token");
+    }
+
+}
+
+export default getArtistTopTracks;
